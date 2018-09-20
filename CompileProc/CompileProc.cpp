fcefa5aee46a44f58d942a5d704cdee1499eb700
pengfugen <pfg0717@163.com>
2018-09-20 15:47:52 +0800

    add

---
+++ CompileProc/CompileProc.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : CompileProc.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+#define PI 3.141596
+
+const int a33 = 33;
+
+int main(void) {
+	int value = PI * 2;
+	int b44 = a33;
+	return 0;
+}
---
+++ CompileProc/CompileProc.s
@@ -0,0 +1,94 @@
+	.file	"CompileProc.cpp"
+	.section .rdata,"dr"
+_ZStL19piecewise_construct:
+	.space 1
+.lcomm _ZStL8__ioinit,1,1
+	.align 4
+_ZL3a33:
+	.long	33
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+.LFB1463:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$48, %rsp
+	.seh_stackalloc	48
+	.seh_endprologue
+	call	__main
+	movl	$6, -4(%rbp)
+	movl	$33, -8(%rbp)
+	movl	$0, %eax
+	addq	$48, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	_Z41__static_initialization_and_destruction_0ii;	.scl	3;	.type	32;	.endef
+	.seh_proc	_Z41__static_initialization_and_destruction_0ii
+_Z41__static_initialization_and_destruction_0ii:
+.LFB1841:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	movl	%ecx, 16(%rbp)
+	movl	%edx, 24(%rbp)
+	cmpl	$1, 16(%rbp)
+	jne	.L5
+	cmpl	$65535, 24(%rbp)
+	jne	.L5
+	leaq	_ZStL8__ioinit(%rip), %rcx
+	call	_ZNSt8ios_base4InitC1Ev
+	movq	.refptr.__dso_handle(%rip), %r8
+	leaq	_ZStL8__ioinit(%rip), %rdx
+	movq	.refptr._ZNSt8ios_base4InitD1Ev(%rip), %rcx
+	call	__cxa_atexit
+.L5:
+	nop
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	_GLOBAL__sub_I_main;	.scl	3;	.type	32;	.endef
+	.seh_proc	_GLOBAL__sub_I_main
+_GLOBAL__sub_I_main:
+.LFB1842:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	movl	$65535, %edx
+	movl	$1, %ecx
+	call	_Z41__static_initialization_and_destruction_0ii
+	nop
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.section	.ctors,"w"
+	.align 8
+	.quad	_GLOBAL__sub_I_main
+	.ident	"GCC: (GNU) 6.4.0"
+	.def	_ZNSt8ios_base4InitC1Ev;	.scl	2;	.type	32;	.endef
+	.def	__cxa_atexit;	.scl	2;	.type	32;	.endef
+	.section	.rdata$.refptr._ZNSt8ios_base4InitD1Ev, "dr"
+	.globl	.refptr._ZNSt8ios_base4InitD1Ev
+	.linkonce	discard
+.refptr._ZNSt8ios_base4InitD1Ev:
+	.quad	_ZNSt8ios_base4InitD1Ev
+	.section	.rdata$.refptr.__dso_handle, "dr"
+	.globl	.refptr.__dso_handle
+	.linkonce	discard
+.refptr.__dso_handle:
+	.quad	__dso_handle
